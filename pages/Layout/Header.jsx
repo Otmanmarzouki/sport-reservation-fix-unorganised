b23b711ea196b2d5d5c4d1a8bbfd96801fefabda
otmanmarzouki <marzouki53@gmail.com>
2024-04-11 22:43:04 +0200

    improve header

--- pages/Layout/Header.jsx
+++ pages/Layout/Header.jsx
@@ -1,43 +1,32 @@
 import React from "react";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import Image from "next/image";
+import logo from "@/public/img/logo.png";
+import avatar from "@/public/img/avatar.jpg";
 
 export default function Header({ handleToggle }) {
   return (
-    <>
-      <div className="flex flex-row bg-white h-14 justify-between px-5">
-        <div className="flex items-center text-black  text-xl font-bold">
-          <button onClick={handleToggle}>test </button>
-          <h1>LOGO</h1>
+    <header className="flex items-center justify-between bg-white h-16 px-5 shadow-lg">
+      <button onClick={handleToggle} className="focus:outline-none">
+        <Image src={logo} width={120} height={120} alt="logo" />
+      </button>
+      <div className="flex items-center space-x-4">
+        <div className="relative">
+          <button className="text-black focus:outline-none">
+            <IoIosNotificationsOutline className="text-xl" />
+          </button>
+          <div className="absolute top-0 right-0 h-3 w-3 bg-blue-600 rounded-full"></div>
         </div>
-        <div className="flex flex-row">
-          <div className="flex flex-row space-x-2 lg:space-x-10">
-            <div className="flex flex-row space-x-3">
-              <div className="flex  justify-center items-center lg:text-xl text-black">
-                <IoIosNotificationsOutline />
-                <div className=" relative">
-                  <div className="absolute bottom-0 left-auto right-0 top-0  inline-block  rounded-full bg-blue-600 p-1 text-xs"></div>
-                </div>
-              </div>
-
-              <div className=" hidden lg:flex flex-col justify-center ">
-                <div className="flex text-black text-xs">
-                  <p>El marzouki</p>
-                </div>
-                <div className="flex text-black text-xs">
-                  <p>Otman</p>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-row lg:space-x-3 justify-center items-center">
-              <img src="/img/ProfilePhoto.jpg" className=" lg:h-14 lg:w-14  w-9 h-9 rounded-full" />
-              <div className="flex justify-center items-center  text-blue-600">
-                <MdKeyboardArrowDown />
-              </div>
-            </div>
+        <div className="flex items-center space-x-2">
+          <Image src={avatar} className="h-12 w-12 rounded-full" alt="User Avatar" />
+          <div className="flex flex-col justify-center">
+            <div className="text-black text-sm font-semibold">El marzouki</div>
+            <div className="text-gray-600 text-xs">Otman</div>
           </div>
+          <MdKeyboardArrowDown className="text-blue-600 h-6 w-6" />
         </div>
       </div>
-    </>
+    </header>
   );
 }
