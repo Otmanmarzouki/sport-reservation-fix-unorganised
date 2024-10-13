c3239e4e2fd8ab914f146d20e35a25c7e06ca4b6
otmanmarzouki <marzouki53@gmail.com>
2024-10-13 14:02:24 +0200

    "Added tooltip and legend customizations to PieChart component"

--- components/Costumer/Statistiques/PieChart/index.jsx
+++ components/Costumer/Statistiques/PieChart/index.jsx
@@ -19,8 +19,41 @@ const SportPieChart = () => {
   const options = {
     responsive: true,
     plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            const dataset = tooltipItem.dataset;
+            const currentValue = dataset.data[tooltipItem.dataIndex];
+            const total = dataset.data.reduce((acc, value) => acc + value, 0);
+            const percentage = ((currentValue / total) * 100).toFixed(1);
+            return `● ${percentage}%`;
+          },
+        },
+      },
       legend: {
         position: "bottom",
+        labels: {
+          usePointStyle: true,
+          boxWidth: 10,
+          padding: 10,
+
+          generateLabels: function (chart) {
+            const { data } = chart;
+            const datasets = data.datasets[0];
+            const total = datasets.data.reduce((acc, value) => acc + value, 0);
+            return data.labels.map((label, index) => {
+              const value = datasets.data[index];
+              const percentage = ((value / total) * 100).toFixed(1);
+              return {
+                text: `${label}: ${percentage}%`,
+                fillStyle: datasets.backgroundColor[index],
+                pointStyle: "circle",
+                borderWidth: 0,
+                strokeStyle: datasets.backgroundColor[index],
+              };
+            });
+          },
+        },
       },
     },
   };
