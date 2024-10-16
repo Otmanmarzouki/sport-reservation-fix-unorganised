4ca7ee9dbb6c1fd71abcc9d637135e3367e40d45
otmanmarzouki <marzouki53@gmail.com>
2024-10-16 19:35:21 +0200

    "Refactor SidebarItem component and update Sidebar layout"

--- pages/Layout/sidebar.jsx
+++ pages/Layout/sidebar.jsx
@@ -41,58 +41,54 @@ export default function Sidebar({ isOpen, toggleSidebar }) {
 
       <div
         className={`fixed lg:static top-0 left-0 h-full lg:h-screen z-50 transition-transform transform bg-orange-400 text-white ${
-          isOpen ? (isMobile ? "w-full translate-x-0" : "lg:w-64") : "-translate-x-full"
+          isOpen ? (isMobile ? "w-full translate-x-0" : "lg:w-56") : "-translate-x-full"
         } lg:translate-x-0`}
       >
-        <div className="p-6">
-          <button className="lg:hidden text-white mb-4" onClick={toggleSidebar}>
+        <div className="p-4 flex flex-col h-full overflow-y-auto">
+          <button className="lg:hidden text-white mb-2">
             <FaTimes />
           </button>
 
-          <ul className="flex flex-col space-y-6">
+          <ul className={`flex flex-col space-y-2 ${isMobile ? "items-start px-2" : ""}`}>
             <li>
-              <SidebarItem icon={<FaHome />} text="Accueil" onClick={toggleSidebar} />
+              <SidebarItem icon={<FaHome />} text="Accueil" />
             </li>
             <li>
-              <SidebarItem icon={<FaCalendarAlt />} text="Réservations" onClick={toggleSidebar} />
+              <SidebarItem icon={<FaCalendarAlt />} text="Réservations" />
             </li>
             <li>
-              <SidebarItem icon={<FaChartBar />} text="Tableau de bord" onClick={toggleSidebar} />
+              <SidebarItem icon={<FaChartBar />} text="Tableau de bord" />
             </li>
             <li>
-              <SidebarItem icon={<FaFutbol />} text="Terrains" onClick={toggleSidebar} />
+              <SidebarItem icon={<FaFutbol />} text="Terrains" />
             </li>
             <li>
-              <SidebarItem icon={<FaUsers />} text="Clients" onClick={toggleSidebar} />
+              <SidebarItem icon={<FaUsers />} text="Clients" />
             </li>
             <li>
-              <SidebarItem
-                icon={<FaMoneyBill />}
-                text="Chiffre d'affaire"
-                onClick={toggleSidebar}
-              />
+              <SidebarItem icon={<FaMoneyBill />} text="Chiffre d'affaire" />
             </li>
             <li>
-              <SidebarItem icon={<FaHistory />} text="Historique" onClick={toggleSidebar} />
+              <SidebarItem icon={<FaHistory />} text="Historique" />
             </li>
           </ul>
-        </div>
 
-        <div className="p-6 mt-24 flex flex-col space-y-6">
-          <ul className="flex flex-col space-y-6">
-            <li>
-              <SidebarItem icon={<FaBullhorn />} text="Marketing" onClick={toggleSidebar} />
-            </li>
-            <li>
-              <SidebarItem icon={<FaHeadset />} text="Support" onClick={toggleSidebar} />
-            </li>
-            <li>
-              <SidebarItem icon={<FaCog />} text="Paramètres" onClick={toggleSidebar} />
-            </li>
-            <li className="mt-6 w-full border-t-2 border-white pt-3">
-              <SidebarItem icon={<FaSignOutAlt />} text="Déconnexion" onClick={toggleSidebar} />
-            </li>
-          </ul>
+          <div className="mt-auto">
+            <ul className={`flex flex-col space-y-2 ${isMobile ? "items-start px-2" : ""}`}>
+              <li>
+                <SidebarItem icon={<FaBullhorn />} text="Marketing" />
+              </li>
+              <li>
+                <SidebarItem icon={<FaHeadset />} text="Support" />
+              </li>
+              <li>
+                <SidebarItem icon={<FaCog />} text="Paramètres" />
+              </li>
+              <li className="mt-2 w-full border-t-2 border-white pt-2">
+                <SidebarItem icon={<FaSignOutAlt />} text="Déconnexion" />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
